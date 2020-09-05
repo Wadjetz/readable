8e40083eb0931a9e2cf2757394cfc5cbeab1e775
Egor <wadjetz@tuta.io>
2020-09-05 19:23:15 +0200

    Add github actions

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import Mercury from "@postlight/mercury-parser"
 
 const app = express()
 
+const port = 3000
+
 app.get("/readable", (req, res) =>
-  Mercury.parse(req.query.url)
+  Mercury.parse(req.query.url as string)
     .then((result) => res.json(result))
     .catch((error) => res.json(error))
 )
 
-app.listen(3000, () => console.log("App listening on port 3000!"))
+app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
