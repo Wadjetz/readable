45d0c9c5de1344a3a23fb57f9e15bb23937e74e0
egor <egor@berezify.fr>
2021-03-21 07:10:57 +0100

    Remove scripts and classes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express from "express"
 import Mercury from "@postlight/mercury-parser"
+import { parseHTML } from "linkedom"
 
 const app = express()
 
-const port = 3000
+const port = 4300
 
 app.get("/readable", (req, res) =>
   Mercury.parse(req.query.url as string)
-    .then((result) => res.json(result))
+    .then((result) => {
+      if (result.content) {
+        const content = parse(result.content)
+        res.json({ ...result, content })
+      } else {
+        res.json(result)
+      }
+    })
     .catch((error) => res.json(error))
 )
+
+function parse(html: string): string {
+  const { document } = parseHTML(html)
+  document.querySelectorAll("script").forEach((script) => script.remove())
+  document.querySelectorAll("*").forEach((element) => element.removeAttribute("class"))
+  return document.toString()
+}
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
